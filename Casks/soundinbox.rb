ca2a17618826d9d1e2b114eccc5ea098afdf1f44
juanatsap <juanatsap@gmail.com>
2026-05-02 15:20:39 +0100

    Add SoundInbox cask v1.2.0

---
+++ Casks/soundinbox.rb
@@ -0,0 +1,19 @@
+cask "soundinbox" do
+  version "1.2.0"
+  sha256 "c7cebed543abd5d71a5e62ef354b061304451ceb6016170db79f0f5b6d74454c"
+
+  url "https://github.com/drolosoft/soundinbox/releases/download/v#{version}/SoundInbox-v#{version}-macOS.dmg"
+  name "SoundInbox"
+  desc "Turn important emails into unmistakable sounds"
+  homepage "https://drolosoft.com/soundinbox"
+
+  depends_on macos: ">= :sonoma"
+
+  app "SoundInbox.app"
+
+  zap trash: [
+    "~/Library/Application Support/SoundInbox",
+    "~/Library/Preferences/com.drolosoft.soundinbox.plist",
+    "~/Library/Caches/com.drolosoft.soundinbox",
+  ]
+end
