578054c8490c421f4329de6056401aa6583b76f2
juanatsap <juanatsap@gmail.com>
2026-05-12 10:09:58 +0100

    Update SoundInbox cask to v1.2.7

--- Casks/soundinbox.rb
+++ Casks/soundinbox.rb
@@ -1,6 +1,6 @@
 cask "soundinbox" do
-  version "1.2.6"
-  sha256 "3bc7c56c8c7afd0f9fac55e1b1f9349b0abdfb2aae8991d411107b1743d44136"
+  version "1.2.7"
+  sha256 "406908ba4f8662e47d5392ec85be239eec9dfdaf5383e477b5056201057deb92"
 
   url "https://github.com/drolosoft/soundinbox/releases/download/v#{version}/SoundInbox-v#{version}-macOS.dmg"
   name "SoundInbox"
